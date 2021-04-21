920553ffd1893b8231af74b97b7255dfba716693
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:31:34 +0530

    feat:Add flash light script

---
+++ FlashLight.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minAngle = 40f;
+
+    Light light;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if(light.spotAngle <= minAngle)
+        {
+            return;
+        }
+        else
+        {
+            light.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        light.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    public void RestoreAngle(float restoreAngle)
+    {
+        light.spotAngle = restoreAngle;
+    }
+    public void RestoreLight(float intensityAmount)
+    {
+        light.intensity = intensityAmount;
+    }
+}

7106210de741fe92cd8409cdb9acd0efa9c7e351
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:28:38 +0530

    feat:Add display damage script

---
+++ DisplauDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DisplauDamage : MonoBehaviour
+{
+    [SerializeField] Canvas impCanvas;
+    [SerializeField] float impactTime = 0.3f;
+    [SerializeField] AudioSource zombieatack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        impCanvas.enabled = false;
+    }
+
+    public void ShowDamageImpact()
+    {
+        StartCoroutine(showSplatter());
+    }
+
+    IEnumerator showSplatter()
+    {
+        zombieatack.Play();
+        impCanvas.enabled = true;
+        yield return new WaitForSeconds(impactTime);
+        impCanvas.enabled = false;
+        zombieatack.Stop();
+    }
+}

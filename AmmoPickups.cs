83f380c6193cd6a8140072fd2d8a616cacd37cd9
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:24:42 +0530

    feat:Add ammo pick script

---
+++ AmmoPickups.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickups : MonoBehaviour
+{
+
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammotype;
+    [SerializeField] AudioSource pickup;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            pickup.Play();
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammotype, ammoAmount);
+            
+            Destroy(gameObject);
+        }
+    }
+}

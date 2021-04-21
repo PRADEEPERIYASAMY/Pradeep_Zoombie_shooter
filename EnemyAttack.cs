48cbd84a9ac62a86f9fa31336f36e1ed30cb9004
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:30:54 +0530

    feat:Add ememy attack script

---
+++ EnemyAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] float damage = 50;
+
+    PlayerHealth target;
+   
+    void Start()
+    {
+        target = FindObjectOfType<PlayerHealth>();
+    }
+    public void AttackHitEvent()
+    {
+        if (target == null) return;
+        target.GetComponent<PlayerHealth>().TakeDamage(damage);
+        target.GetComponent<DisplauDamage>().ShowDamageImpact();
+        
+    }
+
+    
+}

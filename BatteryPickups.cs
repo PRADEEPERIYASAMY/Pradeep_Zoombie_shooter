a642851cbcef8a616fe6876f8634653a92b788d0
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:25:45 +0530

    feat:Add battery pick script

---
+++ BatteryPickups.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickups : MonoBehaviour
+{
+    [SerializeField] float restoreAngle = 90f;
+    [SerializeField] float restoreIntensity = 10f;
+    [SerializeField] AudioSource pickups;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            pickups.Play();
+            other.GetComponentInChildren<FlashLight>().RestoreAngle(restoreAngle);
+            other.GetComponentInChildren<FlashLight>().RestoreLight(restoreIntensity);
+            Destroy(gameObject);
+        }
+    }
+}

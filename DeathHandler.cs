e1f59696c0139a316443342b3b94cf710888b79e
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:28:03 +0530

    feat:Add death handler script

---
+++ DeathHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHandler : MonoBehaviour
+{
+
+    [SerializeField] Canvas gameOver;
+    void Start()
+    {
+        gameOver.enabled = false;
+    }
+
+    public void HandleDeath()
+    {
+        gameOver.enabled = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        FindObjectOfType<Weapon>().enabled = false;
+    }
+
+   
+}

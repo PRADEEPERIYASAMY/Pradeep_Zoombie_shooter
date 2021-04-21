69cc4acd93f0d6890469b647f10a08f301bcb8c9
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:29:09 +0530

    feat:Add endgame script

---
+++ EndGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    [SerializeField] Canvas gameover;
+    private void OnTriggerEnter(Collider other)
+    {
+        gameover.enabled = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
+        FindObjectOfType<Weapon>().enabled = false;
+    }
+}

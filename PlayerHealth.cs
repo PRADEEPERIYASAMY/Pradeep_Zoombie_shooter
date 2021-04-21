6d59eff007efd05fa2a49b4db0318c15de5d528f
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-04-21 09:32:08 +0530

    feat:Add player health script

---
+++ PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+    [SerializeField] TextMeshProUGUI health;
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        health.text = hitPoints.ToString();
+        if (hitPoints <= 0)
+        {
+            GetComponent<DeathHandler>().HandleDeath();
+        }
+    }
+}
